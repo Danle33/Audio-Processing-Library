df489a0b8e3114de8c5cca7c02c4efc716933dae
Danle33 <pokava2005@gmail.com>
2026-08-15 01:13:56 +0200

    added practical main

--- main.cpp
+++ main.cpp
@@ -13,22 +13,40 @@
 
 int main() {
 
-    lib::core::AudioContainer hz440;
-    lib::core::AudioContainer hz480;
-
-    lib::audioGenerator::generateSineWave(hz440, 440, 0.5, 0, 3, 44100);
-    lib::audioGenerator::generateSineWave(hz480, 480, 0.5, 0, 3, 44100);
-
     lib::core::AudioContainer myContainer;
-    auto vec = std::vector<lib::core::AudioContainer>{hz440, hz480};
-    lib::audioGenerator::joinSineWaves(myContainer, vec);
+
+    if (const auto status = lib::io::read("Assets/riff.wav", myContainer)) {
+        std::cout << *status << '\n';
+        return 0;
+    }
+
+    lib::dsp::volumeDecibels(myContainer, -0.5);
+
+    double peakBefore;
+    lib::dsp::measurePeak(myContainer, &peakBefore);
+    std::cout << "Peak before compression: " << peakBefore << '\n';
+
+    double gainReduction;
+    lib::dsp::Compressor compressor(-20, 4, 1, 50, 1);
+    compressor.process(myContainer, &gainReduction);
+    std::cout << "Gain reduction: " << gainReduction << '\n';
+
+    double peakAfter;
+    lib::dsp::measurePeak(myContainer, &peakAfter);
+    std::cout << "Peak after compression: " << peakAfter << '\n';
+
+    lib::dsp::volumeDecibels(myContainer, peakBefore - peakAfter); // Makeup gain
 
     double peak;
     lib::dsp::measurePeak(myContainer, &peak);
-    std::cout << peak << std::endl;
+    std::cout << "Peak after makeup gain: " << peak << '\n';
 
+    lib::dsp::Equalizer eq;
+    eq.highCut(myContainer, 8000, 24, 1);
 
-    if (const auto status = lib::io::write("Assets/joined.wav", myContainer,
+    lib::dsp::stereoToMono(myContainer);
+
+    if (const auto status = lib::io::write("Assets/riffProcessed.wav", myContainer,
                     lib::io::wav::encodingFormat {
                         .sampleRateHz = 44100,
                         .bitDepth = 16,
